d447e8ab2185d1988697d97e1c59c35618d4e48d
NunzioBasciano <nbasho1988@gmail.com>
2024-09-05 23:31:44 +0200

    fix: CopyrightSection

--- src/components/Molecules/CopyrightSection/CopyrightSection.tsx
+++ src/components/Molecules/CopyrightSection/CopyrightSection.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import style from './copyrightSection.module.scss'
+import style from './copyrightSection.module.scss';
+import { labels } from '@/data/labels';
+
 
 function CopyrightSection() {
     return (
-        <div className={style.container}>Ciao</div>
+        <div className={style.container}>
+            <div className={style.copyright}>
+                <h4>
+                    {labels.copyrightLabel}
+                </h4>
+            </div>
+        </div>
     )
 }
 
